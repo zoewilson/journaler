a83d2d787d7f2d971af66222094497996f618b6a
zakrywilson <zakrywilson@gmail.com>
2016-06-19 13:06:41 -0600

    Updated POM for JRE 1.8

---
+++ src/main/java/com/thewilsons/journaler/model/DatabaseManager.java
@@ -0,0 +1,65 @@
+package com.thewilsons.journaler.model;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The database for the Model for saving and extracting posts from file.
+ *
+ * @author Zoe Wilson
+ * @author Zach Wilson
+ * @since version 1.0
+ * created February 22, 2016
+ */
+final class DatabaseManager {
+
+    /** The resources directory. */
+    private File directory = new File("resources");
+
+    /** The file containing all posts. */
+    private File file = new File(directory.getAbsolutePath() + File.separator + "posts.xml");
+
+    /**
+     * Gets the posts from file and returns them. If the file does not exist, the resources directory and the posts.xml
+     * file will be created.
+     * @return the posts.
+     * @throws Exception
+     */
+    List<Post> getPosts() throws Exception {
+
+
+        // Directory and file do not exist
+        if (!directory.exists()) {
+            if (!directory.mkdir()) {
+                throw new Exception("Failed to create '" + directory + "'.");
+            }
+            if (!file.createNewFile()) {
+                throw new Exception("Failed to create '" + file + "'.");
+            }
+            return new ArrayList<>();
+        }
+
+        // Directory exists but file does not exist
+        else {
+            if (!file.exists()) {
+                if (!file.createNewFile()) {
+                    throw new Exception("Failed to create '" + file + "'.");
+                }
+                return new ArrayList<>();
+            }
+        }
+
+        return PostsProcessor.parse(file);
+    }
+
+    /**
+     * Writes the posts back to file.
+     * @param posts the posts to be written to file.
+     * @throws Exception
+     */
+    void writePosts(List<Post> posts) throws Exception {
+        PostsProcessor.write(file, posts);
+    }
+
+}
---
+++ src/main/java/com/thewilsons/journaler/model/MarkdownRenderer.java
@@ -0,0 +1,60 @@
+package com.thewilsons.journaler.model;
+
+import org.markdown4j.Markdown4jProcessor;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Handles parsing markdown for rendering.
+ *
+ * @author Zach Wilson
+ * @since version 1.0
+ * created Mar 4, 2016
+ */
+public final class MarkdownRenderer {
+
+    /** The Markdown processor. */
+    private Markdown4jProcessor processor;
+
+    /**
+     * Constructor.
+     */
+    public MarkdownRenderer() {
+        processor = new Markdown4jProcessor();
+    }
+
+    /**
+     * Converts Markdown text to HTML.
+     * @param post the Markdown text.
+     * @return HTML equivalent of the markdown.
+     */
+    public String markdownToHTML(Post post) {
+        String htmlText;
+        try {
+            htmlText = processor.process(post.getText());
+        } catch (IOException e) {
+            return null;
+        }
+        return "<html>" + htmlText + "</html>";
+    }
+
+    /**
+     * Converts a list of Markdown text to HTML.
+     * @param posts the Markdown text.
+     * @return HTML equivalent of the markdown.
+     */
+    public String markdownToHTML(List<Post> posts) {
+        String htmlText = "";
+        for (Post post : posts) {
+            String text;
+            try {
+                text = processor.process(post.getText());
+            } catch (IOException e) {
+                return null;
+            }
+            htmlText += text;
+        }
+        return "<html>" + htmlText + "</html>";
+    }
+
+}
