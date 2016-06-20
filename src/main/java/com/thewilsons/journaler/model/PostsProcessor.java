4a1e84c0d89bfbb187831080dc75aece2ad5ce74
zakrywilson <zakrywilson@gmail.com>
2016-06-19 19:54:08 -0600

    Fixing logging

--- src/main/java/com/thewilsons/journaler/model/PostsProcessor.java
+++ src/main/java/com/thewilsons/journaler/model/PostsProcessor.java
@@ -51,10 +51,9 @@ final class PostsProcessor {
 
         // For each post
         for (Node postNode = allPosts.getFirstChild();
-             (postNode = postNode.getNextSibling()) != null;
-              postNode = postNode.getNextSibling())
+                 (postNode = postNode.getNextSibling()) != null;
+                  postNode = postNode.getNextSibling())
         {
-
             // Get the time stamp
             Node timeNode = postNode.getFirstChild().getNextSibling();
             if (!timeNode.getNodeName().equals("time")) {
@@ -76,9 +75,7 @@ final class PostsProcessor {
             // Add it
             posts.add(new Post(timeNode.getTextContent(), textNode.getTextContent()));
             if (LOG.isDebugEnabled()) {
-                Post p = posts.get(posts.size()-1);
-                LOG.debug("Retrieved new post: Post[time=" + p.getTime() + ", text=" +
-                          p.getText().substring(0,25));
+                LOG.debug("Retrieved new post: " + posts.get(posts.size()-1));
             }
         }
 
